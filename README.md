34e2ae45f0ca05fb2ca0e8c03aad2a3ecfcc29fc
Rahadian <rahadian@vouch.sg>
2021-04-19 12:52:27 +0700

    first

---
+++ README.md
@@ -0,0 +1,21 @@
+## Setup
+
+To setup this project, you must have `node` and `npm` installed, then run this command:
+
+```
+npm install
+```
+
+## Environment
+
+```
+AWS_ACCESS_KEY_ID={{access_key_id}}
+AWS_SECRET_ACCESS_KEY={{secret_access_key}}
+AWS_S3_BUCKET={{bucket_name}}   
+```
+
+## Run
+
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+const fs = require('fs')
+const key = `GITS - 2021-03.zip`
+
+S3.getObject({
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: 'GITS - 2021-03.zip'
+}).promise().then(result => {
+    console.log(result)
+
+    fs.writeFileSync(key, result.Body)
+    console.log('done!')
+
+}).catch(e => console.log('error', e))
